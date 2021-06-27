39f22c140666f80203f4cd73bbcac7a4bf3cea6f
TanmayBhilkar <86276947+TanmayBhilkar@users.noreply.github.com>
2021-06-27 11:05:22 +0530

    Add files via upload

---
+++ NUMBER.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int main()
+{
+    int number,guess,nguesses=1 ;
+    srand(time(0));
+    number= rand()%100+1;//generate the randome number in between 1  and  100
+    //printf("tyhe number is %d\n", number);
+    // keep the loop untill the no is guessed
+
+
+    do{
+        printf("guess th nmber between 1 to 100\n");
+        scanf("%d", &guess);
+        if(guess>number)
+        {
+            printf("lower number please\n");
+
+        }
+        else if (guess<number)
+        {
+            printf("higher number please\n");
+        }
+        else{
+            printf("you guessed number in %d attempts\n",nguesses);
+        }
+        nguesses++;
+    }
+    while(guess!=number);
+    return 0;
+}
